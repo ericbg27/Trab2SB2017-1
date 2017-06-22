689e7db72a2587b52e0f3f335d16711454db1cb3
ericbg27 <ericbgil@gmail.com>
2017-06-22 02:50:43 -0300

    Add files via upload

--- Escreve_Inteiro.s
+++ Escreve_Inteiro.s
@@ -8,93 +8,92 @@ N1 resd 1
 section .text
 global _start
 _start:
-mov dword [N1],123
-push dword [N1]
+mov dword [N1],1234567890
+push N1
 call Escreve_Inteiro
 mov eax,1
 mov ebx,0
 int 80h
-Escreve_Inteiro: enter 9,0
-				 push eax
-				 push ebx
-				 push ecx
-				 push edx
-				 push esi
-				 push edi
-				 mov ebx,0 ;Flag
-				 mov esi,0 ;contador
-				 mov ecx,10
-				 mov eax,[EBP+8]
-				 cmp dword [EBP+8],0 ;Verificando se o numero eh negativo
-				 jl Negativo
-				 Positivo: mov edx,0
-				 		   cmp eax,0
-				 		   je Imprime
-				 		   div ecx
-				 		   add edx,0x30 ;(Valor%10) + 0x30
-				 		   cmp eax,0
-				 		   cmp edx,0x30
-				 		   jl Numero_Inv
-				 		   cmp edx,0x39
-				 		   jg Numero_Inv
-				 		   mov [EBP-9+esi],edx
-				 		   inc esi
-				 		   jmp Positivo
-				 Negativo: mov ebx,1
-				 		   neg eax
-				 		   Neg_Loop: mov edx,0
-				 		   			 cmp eax,0
-				 		   			 je Imprime
-				 		   			 div ecx
-				 		   			 add edx,0x30 ;(Valor%10) + 0x30
-				 		   			 cmp eax,0
-				 		   			 cmp edx,0x30
-				 		   			 jl Numero_Inv
-				 		   			 cmp edx,0x39
-				 		   			 jg Numero_Inv
-				 		   			 mov [EBP-9+esi],dl
-				 		   			 inc esi
-				 		   			 jmp Neg_Loop
-				 Numero_Inv: mov eax,4
-                        	 mov ebx,1
-                        	 mov ecx,msg0
-                        	 mov edx,17
-                        	 int 80h
-                        	 jmp Exit_Func
+Escreve_Inteiro: enter 11,0
+                 push eax
+                 push ebx
+                 push ecx
+                 push edx
+                 push esi
+                 push edi
+                 mov ebx,0 ;Flag
+                 mov esi,0 ;contador
+                 mov ecx,10
+                 mov edi,[EBP+8]
+                 mov eax,[edi]
+                 cmp dword [edi],0 ;Verificando se o numero eh negativo
+                 jl Neg
+         Positivo: cdq 
+               cmp eax,0
+               je Imprime
+               div ecx
+               add edx,0x30 ;(Valor%10) + 0x30
+               cmp edx,0x30
+               jl Numero_Inv
+               cmp edx,0x39
+               jg Numero_Inv
+               mov [EBP-11+esi],edx
+               inc esi
+               jmp Positivo
+         Neg: mov ebx,1
+               neg eax
+               Neg_Loop: cdq
+                     cmp eax,0
+                     je Imprime
+                     div ecx
+                     add edx,0x30 ;(Valor%10) + 0x30
+                     cmp edx,0x30
+                     jl Numero_Inv
+                     cmp edx,0x39
+                     jg Numero_Inv
+                     mov [EBP-11+esi],dl
+                     inc esi
+                     jmp Neg_Loop
+         Numero_Inv: mov eax,4
+                     mov ebx,1
+                     mov ecx,msg0
+                     mov edx,17
+                     int 80h
+                     jmp Exit_Func
                  Imprime: cmp ebx,1
-                 		  je insert_sign
-                 		  mov byte [EBP-9+esi],0ah
-                 		  mov edi,esi
-                 		  mov eax,9
-                 		  sub eax,esi
-                 		  mov edi,eax
-                 		  mov esi,eax
-                 		  inc esi
-                 		  Sys_Write: mov eax,4
-                 		  		     mov ebx,1 
-                 		             mov ecx,EBP
-									 sub ecx,esi
-                 		             mov edx,1
-                 		             int 80h
-                 		             inc esi
-                 		             cmp esi,10
-                 		             jne Sys_Write
-                 		             mov eax,4
-                 		             mov ebx,1
-                 		             mov ecx,EBP
-                 		             sub ecx,edi
-                 		             mov edx,1
-                 		             int 80h
-                 		             jmp Exit_Func
-                 insert_sign: mov byte [EBP-9+esi],0x2D
-                 			  inc esi
-                 			  mov ebx,0
-                 			  jmp Imprime
-                 Exit_Func: pop esi
-                 			pop edx
-                 			pop ecx
-                 			pop ebx
-                 			pop eax
-                 			leave
-                 			ret
-				 
+                      je insert_sign
+                      mov byte [EBP-11+esi],0ah
+                      mov edi,esi
+                      mov eax,11
+                      sub eax,esi
+                      mov edi,eax
+                      mov esi,eax
+                      inc esi
+                      Sys_Write: mov eax,4
+                                 mov ebx,1 
+                                 mov ecx,EBP
+                                 sub ecx,esi
+                                 mov edx,1
+                                 int 80h
+                                 inc esi
+                                 cmp esi,12
+                                 jne Sys_Write
+                                 mov eax,4
+                                 mov ebx,1
+                                 mov ecx,EBP
+                                 sub ecx,edi
+                                 mov edx,1
+                                 int 80h
+                                 jmp Exit_Func
+                 insert_sign: mov byte [EBP-11+esi],0x2D
+                              inc esi
+                              mov ebx,0
+                              jmp Imprime
+                 Exit_Func: pop edi
+                            pop esi
+                            pop edx
+                            pop ecx
+                            pop ebx
+                            pop eax
+                            leave
+                            ret
--- Leer_Inteiro.asm
+++ Leer_Inteiro.asm
@@ -1,3 +1,4 @@
+;**********************************Verificar para o 1*****************************************
 %include "io.mac"
 section .data
 msg0 db "Numero Invalido!",0ah
@@ -9,12 +10,12 @@ global _start
 _start:
 push N1
 call Leer_Inteiro
-PutInt [N1]
-PutCh 0ah
+push N1
+call Escreve_Inteiro
 mov eax,1
 mov ebx,0
 int 80h
-Leer_Inteiro: enter 14,0
+Leer_Inteiro: enter 20,0 
               push eax
               push ebx
               push ecx
@@ -22,26 +23,25 @@ Leer_Inteiro: enter 14,0
               push esi
               push edi
               mov esi,0 ;contador
-              mov word [EBP-14],0 ;Zerando flag
+              mov word [EBP-20],0 ;Zerando flag
               mov eax,3
               mov ebx,0
               mov ecx,EBP
-              sub ecx,12
-              mov edx,6
+              sub ecx,18
+              mov edx,11
               int 80h
               mov ecx,eax
-              sub ecx,1 ;ecx = numero de caracteres - 1
+              sub ecx,1 
               mov eax,0 ;Zerando acumulador
               mov edi,10
               mov ebx,0
-              cmp byte [EBP-12],0x2D
+              cmp byte [EBP-18],0x2D
               je Negativo
-              Convert_Int: cmp byte [EBP-12+esi],0x30
+              Convert_Int: cmp byte [EBP-18+esi],0x30
                            jl Invalido
-                           cmp byte [EBP-12+esi],0x39
+                           cmp byte [EBP-18+esi],0x39
                            jg Invalido
-                           cmp esi,0
-                           mov bl,[EBP-12+esi]
+                           mov bl,[EBP-18+esi]
                            mul edi
                            add eax,ebx
                            sub eax,0x30
@@ -50,7 +50,7 @@ Leer_Inteiro: enter 14,0
               jmp Armazena_Num
               Negativo: inc esi
                         dec ecx
-                        mov word [EBP-14],1 ;flag
+                        mov word [EBP-20],1 ;flag
                         jmp Convert_Int
               Invalido: mov eax,4
                         mov ebx,1
@@ -58,15 +58,15 @@ Leer_Inteiro: enter 14,0
                         mov edx,17
                         int 80h
                         jmp FIM
-              Armazena_Num: cmp word [EBP-14],1
+              Armazena_Num: cmp word [EBP-20],1
                             je Nega_Num
+                            mov esi,eax
                             mov ebx,[EBP+8]
                             mov [ebx],eax ;Armazenando numero convertido na memoria
-                            PutInt AX
-                            PutCh 0ah
+                            int 80h
                             jmp FIM
               Nega_Num: neg eax
-                        mov word [EBP-14],0
+                        mov word [EBP-20],0
                         jmp Armazena_Num
               FIM: pop edi
                    pop esi
@@ -76,3 +76,86 @@ Leer_Inteiro: enter 14,0
                    pop eax
                    leave
                    ret 4
+Escreve_Inteiro: enter 11,0
+                 push eax
+                 push ebx
+                 push ecx
+                 push edx
+                 push esi
+                 push edi
+                 mov ebx,0 ;Flag
+                 mov esi,0 ;contador
+                 mov ecx,10
+                 mov edi,[EBP+8]
+                 mov eax,[edi]
+                 cmp dword [edi],0 ;Verificando se o numero eh negativo
+                 jl Neg
+         Positivo: cdq 
+               cmp eax,0
+               je Imprime
+               div ecx
+               add edx,0x30 ;(Valor%10) + 0x30
+               cmp edx,0x30
+               jl Numero_Inv
+               cmp edx,0x39
+               jg Numero_Inv
+               mov [EBP-11+esi],edx
+               inc esi
+               jmp Positivo
+         Neg: mov ebx,1
+               neg eax
+               Neg_Loop: cdq
+                     cmp eax,0
+                     je Imprime
+                     div ecx
+                     add edx,0x30 ;(Valor%10) + 0x30
+                     cmp edx,0x30
+                     jl Numero_Inv
+                     cmp edx,0x39
+                     jg Numero_Inv
+                     mov [EBP-11+esi],dl
+                     inc esi
+                     jmp Neg_Loop
+         Numero_Inv: mov eax,4
+                     mov ebx,1
+                     mov ecx,msg0
+                     mov edx,17
+                     int 80h
+                     jmp Exit_Func
+                 Imprime: cmp ebx,1
+                      je insert_sign
+                      mov byte [EBP-11+esi],0ah
+                      mov edi,esi
+                      mov eax,11
+                      sub eax,esi
+                      mov edi,eax
+                      mov esi,eax
+                      inc esi
+                      Sys_Write: mov eax,4
+                                 mov ebx,1 
+                                 mov ecx,EBP
+                                 sub ecx,esi
+                                 mov edx,1
+                                 int 80h
+                                 inc esi
+                                 cmp esi,12
+                                 jne Sys_Write
+                                 mov eax,4
+                                 mov ebx,1
+                                 mov ecx,EBP
+                                 sub ecx,edi
+                                 mov edx,1
+                                 int 80h
+                                 jmp Exit_Func
+                 insert_sign: mov byte [EBP-11+esi],0x2D
+                              inc esi
+                              mov ebx,0
+                              jmp Imprime
+                 Exit_Func: pop edi
+                            pop esi
+                            pop edx
+                            pop ecx
+                            pop ebx
+                            pop eax
+                            leave
+                            ret
